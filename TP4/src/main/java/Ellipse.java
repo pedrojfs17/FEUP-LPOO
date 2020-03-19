13bcd8f9ca25b00595955cc2267d4d6f64c75091
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-03-19 19:09:43 +0000

    Started tp4

---
+++ TP4/src/main/java/Ellipse.java
@@ -0,0 +1,20 @@
+public class Ellipse extends Shape {
+    private double xradius;
+    private double yradius;
+
+    public double getXradius() {
+        return xradius;
+    }
+
+    public void setXradius(double xradius) {
+        this.xradius = xradius;
+    }
+
+    public double getYradius() {
+        return yradius;
+    }
+
+    public void setYradius(double yradius) {
+        this.yradius = yradius;
+    }
+}
