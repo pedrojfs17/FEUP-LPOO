7337abe96d16ae4434fc65efc81ebd1d721c0d9f
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-03-20 11:12:36 +0000

    Liskov Substitution

---
+++ TP4/src/main/java/Line.java
@@ -0,0 +1,15 @@
+public class Line implements BasicShape {
+    private double length;
+
+    Line(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+}
