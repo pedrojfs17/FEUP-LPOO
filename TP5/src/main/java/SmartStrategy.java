42c03a46371d14288ab833944987df1a8c5f767b
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-03-27 17:49:04 +0000

    Factory-method Pattern

--- TP5/src/main/java/SmartStrategy.java
+++ TP5/src/main/java/SmartStrategy.java
@@ -1,20 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmartStrategy implements OrderingStrategy {
-    private StringDrink drink;
-    private StringRecipe recipe;
+    private List<StringDrink> drinks;
+    private List<StringRecipe> recipes;
+
+    public SmartStrategy() {drinks = new ArrayList<>(); recipes = new ArrayList<>(); }
 
     @Override
     public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
         if (bar.isHappyHour())
             bar.order(drink, recipe);
         else {
-            this.drink = drink;
-            this.recipe = recipe;
+            this.drinks.add(drink);
+            this.recipes.add(recipe);
         }
     }
 
     @Override
     public void happyHourStarted(StringBar bar) {
-        bar.order(drink, recipe);
+        if (!drinks.isEmpty() && !recipes.isEmpty()) {
+            bar.order(drinks.get(0), recipes.get(0));
+            drinks.remove(0);
+            recipes.remove(0);
+        }
     }
 
     @Override
