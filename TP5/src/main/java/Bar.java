9220782304b1224ce051af4ce8ad27d307f90acc
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-03-27 17:01:18 +0000

    Observer Pattern

---
+++ TP5/src/main/java/Bar.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Bar {
+    private boolean happyHour;
+    private List<BarObserver> observers;
+
+    public Bar() {happyHour = false; observers = new ArrayList<>(); }
+
+    public boolean isHappyHour() {return happyHour;}
+    public void startHappyHour() {happyHour = true; notifyObservers();}
+    public void endHappyHour() {happyHour = false; notifyObservers();}
+
+    public void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
+    }
+}
